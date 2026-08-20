701affecc9a2380f30322912b5f3443d02b1c19d
SniFDev <a.makarevich1993@gmail.com>
2026-08-20 22:49:33 +0300

    Removed

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header({ source }) {
-  return (
-    <header className="app-header">
-      <h1>Occupancy Dashboard</h1>
-      {source && <p className="app-header__source">{source}</p>}
-    </header>
-  )
-}
